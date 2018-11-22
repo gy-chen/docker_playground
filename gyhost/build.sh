7dbbd4bcf13647fbc034407d7bc38cd6746b5033
GYCHEN <gy.chen@gms.nutc.edu.tw>
2018-11-22 16:19:08 +0800

    update

--- gyhost/build.sh
+++ gyhost/build.sh
@@ -1,3 +1,11 @@
+rm matomo.zip
+rm -rf ./matomo/matomo
 curl https://builds.matomo.org/matomo.zip > matomo.zip
 unzip matomo.zip -d matomo
+chown -R www-data:www-data matomo/matomo
+
+rm -rf ./ssl/matomo.gyhost.icu
+mkdir -p ./ssl/matomo.gyhost.icu
+cp /etc/letsencrypt/live/matomo.gyhost.icu/* ./ssl/matomo.gyhost.icu
+
 docker-compose build
